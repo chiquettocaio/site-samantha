7956256fc1facae80b44f55d4c093c06d5a72b62
Caio Chiquetto <34581188+chiquettocaio@users.noreply.github.com>
2026-06-16 20:17:37 -0300

    feat: displaying expertise section

--- personal-website/src/app/app.ts
+++ personal-website/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core'
 import { Header } from './features/landing/components/header/header'
+import { Expertise } from './features/landing/expertise/expertise'
 import { Hero } from './features/landing/hero/hero'
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Hero],
+  imports: [Header, Hero, Expertise],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
