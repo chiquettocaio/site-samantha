ae204fe46138925f60e69520d767d42dbef474bb
Caio Chiquetto <34581188+chiquettocaio@users.noreply.github.com>
2026-06-19 00:27:55 -0300

    feat: responsiveness, code consistency

--- personal-website/src/app/app.ts
+++ personal-website/src/app/app.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core'
 import { BookAppointment } from './features/landing/book-appointment/book-appointment'
 import { Header } from './features/landing/components/header/header'
 import { Expertise } from './features/landing/expertise/expertise'
+import { Faq } from './features/landing/faq/faq'
 import { Hero } from './features/landing/hero/hero'
 
 @Component({
@@ -10,7 +11,8 @@ import { Hero } from './features/landing/hero/hero'
     Header,
     Hero,
     Expertise,
-    BookAppointment
+    BookAppointment,
+    Faq
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
