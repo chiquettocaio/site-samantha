c20da576f1da11ff7a5e787ed668e3c422014c19
Caio Chiquetto <34581188+chiquettocaio@users.noreply.github.com>
2026-06-19 00:27:10 -0300

    feat: faq section creation

---
+++ personal-website/src/app/features/landing/faq/faq.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+import { Icon } from '../../../shared/components/icon/icon'
+
+@Component({
+  selector: 'app-faq',
+  imports: [Icon],
+  templateUrl: './faq.html',
+  styleUrl: './faq.scss'
+})
+export class Faq { }
