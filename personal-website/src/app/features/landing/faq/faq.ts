c7faf9dea6b9b5c6a4daa15c25904fd09b804118
Caio Chiquetto <34581188+chiquettocaio@users.noreply.github.com>
2026-06-20 22:13:25 -0300

    feat: CTAs adjustments

--- personal-website/src/app/features/landing/faq/faq.ts
+++ personal-website/src/app/features/landing/faq/faq.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core'
+import { Button } from '../../../shared/components/button/button'
+import { ButtonVariant } from '../../../shared/components/button/button.model'
 import { Icon } from '../../../shared/components/icon/icon'
 
 @Component({
   selector: 'app-faq',
-  imports: [Icon],
+  imports: [Icon, Button],
   templateUrl: './faq.html',
   styleUrl: './faq.scss'
 })
-export class Faq { }
+export class Faq {
+  ButtonVariant = ButtonVariant
+}
